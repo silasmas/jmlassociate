6b2ad2d622f3e27d27181a6c5793700188307cce
silasmas <silasjmas@gmail.com>
2024-08-20 02:40:36 +0100

    Update expertise.blade.php

--- resources/views/pages/expertise.blade.php
+++ resources/views/pages/expertise.blade.php
@@ -13,12 +13,7 @@
                     <div class="Title-area">
                         <h3>Want Help</h3>
                         <h2>@lang('info.expertises.menu')</h2>
-                        <p> businesses, individuals, and local governments too.is abusiness theme perfectly suited legal advisers and offices, lawyers, attorneys, counsels, advocates and other legal and law related services.</p>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-6">
-                    <div class="asset-button btn-text-left text-right top-margin">
-                        <button type="button" class="btn btn-default">View All Practice Areas</button>
+                        {{-- <p> businesses, individuals, and local governments too.is abusiness theme perfectly suited legal advisers and offices, lawyers, attorneys, counsels, advocates and other legal and law related services.</p> --}}
                     </div>
                 </div>
             </div>
@@ -29,7 +24,7 @@
         <div class="row">
             <div class="h-2-practice-content clearfix">
                 @forelse ($secteur as $s)
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-6 col-md-3">
                     <div class="h-2-p-c-details">
                         <div class="h-2-p-c-default h-3-p-c-default">
                             <img src="{{file_exists('storage/'.$s->photo)?asset("storage/".$s->photo):asset('img/def.jpeg') }}" class="img-responsive" alt="">
@@ -52,12 +47,7 @@
                     <div class="Title-area">
                         <h3>Want Help</h3>
                         <h2>@lang('info.domaine.menu')</h2>
-                        <p>In Trust, we have got a wide range of legal services. We practice general civil law for businesses, individuals, and local governments too.is abusiness theme perfectly suited legal advisers and offices, lawyers, attorneys, counsels, advocates and other legal and law related services.</p>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-6">
-                    <div class="asset-button btn-text-left text-right top-margin">
-                        <button type="button" class="btn btn-default">View All Practice Areas</button>
+                        {{-- <p>In Trust, we have got a wide range of legal services. We practice general civil law for businesses, individuals, and local governments too.is abusiness theme perfectly suited legal advisers and offices, lawyers, attorneys, counsels, advocates and other legal and law related services.</p> --}}
                     </div>
                 </div>
             </div>
@@ -67,12 +57,12 @@
         <div class="row">
             <div class="h-2-practice-content clearfix">
                 @forelse ($domaine as $s)
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-6 col-md-3">
                     <div class="h-2-p-c-details">
                         <div class="h-2-p-c-default h-3-p-c-default">
                             <img src="{{file_exists('storage/'.$s->photo)?asset("storage/".$s->photo):asset('img/def.jpeg') }}" class="img-responsive" alt="">
                             <h2><a href="{{ route('detailExpertise',['id'=>$s->id,'p'=>$s->titre1]) }}">{{ $s->titre1 }}</a></h2>
-                            <p>{!! Str::limit($s->contenu, 100, '...')  !!}</p>
+                            <p>{!! Str::limit($s->contenu, 200, '...')  !!}</p>
                             <a href="{{ route('detailExpertise',['id'=>$s->id,'p'=>$s->titre1]) }}">Voir en detail<i class="fa fa-long-arrow-right"></i></a>
                         </div>
                     </div>
